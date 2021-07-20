6e4dd350224e23683b39e1d711cb5d4481cdfcbc
Ričards Mucelāns <hello@richardev.com>
2021-07-20 19:53:29 +0300

    vue.config updates 3rd time

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,18 @@ const extraWatch = require("extra-watch-webpack-plugin")
 
 module.exports = {
 	devServer: {
+		host: "0.0.0.0",
+		hot: true,
+		disableHostCheck: true,
 		port: process.env.VUE_APP_PORT,
 	},
 	configureWebpack: {
 		devServer: {
+			host: "0.0.0.0",
+			hot: true,
 			disableHostCheck: true,
 			historyApiFallback: true,
-			// public: process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_PORT,
+			port: process.env.VUE_APP_PORT,
 		},
 		plugins: [
 			new extraWatch({
