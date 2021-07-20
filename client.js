d1fe8a7556c96b9560cfa334398afe9b257cb82a
Ričards Mucelāns <hello@richardev.com>
2021-07-20 17:13:43 +0300

    fixed CORS

--- client.js
+++ client.js
@@ -1,10 +1,19 @@
+const bodyParser = require("body-parser")
+const cors = require("cors")
 const dotenv = require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
 const path = require("path")
-const appHost = process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : "0.0.0.0"
+const appHost = process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : "localhost"
 const appPort = process.env.VUE_APP_PORT ? process.env.VUE_APP_PORT : 8080
 
 const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(morgan("dev")) // configire morgan
 app.use(express.static(path.join(__dirname, "dist")))
 
 app.get("*", (req, res) => {
--- server/run.js
+++ server/run.js
@@ -33,7 +33,13 @@ if (dbUrl) {
 			app.use(bodyParser.json())
 
 			app.use(morgan("dev")) // configire morgan
-
+			app.use(function(req, res, next) {
+				res.setHeader("Access-Control-Allow-Origin", "*")
+				res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+				res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+				res.setHeader("Access-Control-Allow-Credentials", true)
+				next()
+			})
 			// Return motd on abspath GET request
 			app.get("/", (req, res) => {
 				res.json({
